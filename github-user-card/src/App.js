f2e9a85ff9fb9fe6caa344649605522d9c85dd07
djviodes <djviodes26@gmail.com>
2020-10-27 16:07:36 -0700

    On the right tract with App.js

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,10 +2,48 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    users: [],
+    userId: ''
+  }
+
+  componentDidMount() {
+    this.fetchUsers('djviodes');
+  }
+
+  fetchUsers = (user) => {
+    axios.get(`https://api.github.com/users/${user}`)
+      .then(response => {
+        this.setState({
+          users: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  handleChange = (event) => {
+    this.setState({ userId: event.target.value });
+  }
+
+  handleSearch = (event) => {
+    event.preventDefault();
+    this.fetchUsers(this.state.userId);
+    this.setState({ userId: '' });
+  }
+
   render() {
     return(
       <div className='App'>
         <h1>Search Github Users</h1>
+        <form onSubmit={this.handleSearch}>
+          <input value={this.state.userId} onChange={this.handleChange} type='text' />
+          <button>Fetch Users</button>
+        </form>
+        <div className='userContainer'>
+          <p>{this.state.name}</p>
+        </div>
       </div>
     )
   }
